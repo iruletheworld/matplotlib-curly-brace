03d290882067ee82c5ad2cdccb8f3bdf22ac6bc2
iruletheworld <siyu.gao@outlook.com>
2019-04-22 21:01:03 +0100

    documentation & improvement

--- exp02.py
+++ exp02.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*- 
 
 '''
+Author : 高斯羽 博士 (Dr. GAO, Siyu)
+
+Version : 1.0.0
+
+Last Modified : 2019-04-22
+
+This script is an example to demonstration how to use the "curlyBrace" module to 
+plot curly brackets using matplotlib.
+
+This example demonstrate annotating two pairs of concentrate circles with the auto scale off.
 '''
 
 import matplotlib.pyplot as plt
 import numpy as np
-from curbrac import curbrac
+from curlyBrace import curlyBrace
 
 # figure size and dpi
 dbl_width   = 1000.0
@@ -30,9 +40,7 @@ y1 = r1 * np.sin(theta)
 x2 = r2 * np.cos(theta)
 y2 = r2 * np.sin(theta)
 
-# fig1, axes1 = plt.subplots(1, 1, figsize=(dbl_width / dbl_dpi, dbl_height / dbl_dpi), dpi=dbl_dpi)
 fig, axes = plt.subplots(2, 1, figsize=(dbl_width / dbl_dpi, dbl_height / dbl_dpi), dpi=dbl_dpi)
-# fig2, axes2 = plt.subplots(1, 1, figsize=(dbl_width / dbl_dpi, dbl_height / dbl_dpi), dpi=dbl_dpi)
 
 # fontdict for axis titles
 font = {'family': 'Arial',
@@ -42,23 +50,24 @@ font = {'family': 'Arial',
         'size': 12,
         }
 
-str_title1 = 'Example: Anti-Clockwise for a a pair of concentrate circles'
-str_title2 = 'Example: Clockwise for a a pair of concentrate circles'
+str_title1 = 'Example: Anti-Clockwise, equal aspect, auto scale off'
+str_title2 = 'Example: Clockwise, equal aspect, auto scale off'
 
 axes[0].plot(x1, y1, lw=lw, color=color)
 axes[0].plot(x2, y2, lw=lw, color=color)
-axes[1].plot(x1, y1, lw=lw, color=color)
-axes[1].plot(x2, y2, lw=lw, color=color)
-
 axes[0].set_aspect('equal', 'box')
 axes[0].grid(color='lightgray', linestyle='--')
 axes[0].set_title(str_title1, fontdict=font)
+
+axes[1].plot(x1, y1, lw=lw, color=color)
+axes[1].plot(x2, y2, lw=lw, color=color)
 axes[1].set_aspect('equal', 'box')
 axes[1].grid(color='lightgray', linestyle='--')
 axes[1].set_title(str_title2, fontdict=font)
 
 # bracket coefficient
 k_r = 0.1
+lw = 3
 
 # points for the brackets
 phi = np.linspace(0, 2.0 * np.pi, 13)
@@ -77,8 +86,6 @@ font = {'family': 'serif',
         'size': 9,
         }
 
-lw = 3
-
 for i in range(0, len(x1)):
 
     p1 = [x1[i], y1[i]]
@@ -86,7 +93,7 @@ for i in range(0, len(x1)):
 
     str_text = 'Circle\nanti-clockwise'
 
-    theta, pt = curbrac(axes[0], p1, p2, k_r, str_text=str_text, color='r', lw=lw, int_line_num=2, fontdict=font)
+    curlyBrace(fig, axes[0], p1, p2, k_r, bool_auto=False, str_text=str_text, color='r', lw=lw, int_line_num=2, fontdict=font)
 
 for i in range(0, len(x1)):
 
@@ -95,6 +102,6 @@ for i in range(0, len(x1)):
 
     str_text = 'Circle\nclockwise'
 
-    theta, pt = curbrac(axes[1], p1, p2, k_r, str_text=str_text, color='r', lw=lw, int_line_num=2, fontdict=font)
+    curlyBrace(fig, axes[1], p1, p2, k_r, bool_auto=False, str_text=str_text, color='darkorange', lw=lw, int_line_num=2, fontdict=font)
 
 plt.show()
