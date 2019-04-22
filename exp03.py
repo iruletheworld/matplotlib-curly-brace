c575a710be58d7c54a26fa0b5687153677a5c029
iruletheworld <siyu.gao@outlook.com>
2019-04-22 21:09:47 +0100

    documentation & improvement

--- exp03.py
+++ exp03.py
@@ -1,29 +1,40 @@
 # -*- coding: utf-8 -*- 
 
 '''
-# epllise
-# https://stackoverflow.com/questions/10952060/plot-ellipse-with-matplotlib-pyplot-python
+Author : 高斯羽 博士 (Dr. GAO, Siyu)
+
+Version : 1.0.0
+
+Last Modified : 2019-04-22
+
+This script is an example to demonstration how to use the "curlyBrace" module to 
+plot curly brackets using matplotlib.
+
+This example demonstrate annotating two pairs of ellipses.
+
+The parametric equation of ellipse can be found:
+https://stackoverflow.com/questions/10952060/plot-ellipse-with-matplotlib-pyplot-python
 '''
 
 import matplotlib.pyplot as plt
 import numpy as np
-from curbrac import curbrac
+from curlyBrace import curlyBrace
 
 # figure size and dpi
 dbl_width   = 1000.0
-dbl_height  = 720.0
+dbl_height  = 800.0
 dbl_dpi     = 100.0
 
-# line width and colour for the epllise
+# line width and colour for the ellipses
 lw = 2
 color='royalblue'
 
 u = 0.0  # x-position of the centre
 v = 0.0  # y-position of the centre
-a1 = 2.0  # radius on the x-axis for epllise 1
-b1 = 1.5  # radius on the y-axis for epllise 1
-a2 = 7.0  # radius on the x-axis for epllise 2
-b2 = 5.5  # radius on the y-axis for epllise 2
+a1 = 2.0  # radius on the x-axis for ellipse 1
+b1 = 1.5  # radius on the y-axis for ellipse 1
+a2 = 7.0  # radius on the x-axis for ellipse 2
+b2 = 5.5  # radius on the y-axis for ellipse 2
 
 theta = np.linspace(0, 2.0 * np.pi, 101)
 
@@ -42,8 +53,8 @@ font = {'family': 'Arial',
         'size': 12,
         }
 
-str_title1 = 'Example: Anti-Clockwise for a a pair of concentrate epllise'
-str_title2 = 'Example: Clockwise for a a pair of concentrate epllise'
+str_title1 = 'Example: Axes aspect equal, auto scale off'
+str_title2 = 'Example: Axes aspect equal, auto scale on'
 
 axes[0].plot(x1, y1, lw=lw, color=color)
 axes[0].plot(x2, y2, lw=lw, color=color)
@@ -78,7 +89,8 @@ font = {'family': 'serif',
 
 # bracket line width and colour
 lw = 3
-color = 'r'
+color1 = 'r'
+color2 = 'darkorange'
 
 for i in range(0, len(x1)):
 
@@ -87,7 +99,7 @@ for i in range(0, len(x1)):
 
     str_text = 'Epllise\nanti-clockwise'
 
-    theta, pt = curbrac(axes[0], p1, p2, k_r, str_text=str_text, color=color, lw=lw, int_line_num=3, fontdict=font)
+    curlyBrace(fig, axes[0], p1, p2, k_r, bool_auto=False, str_text=str_text, color=color1, lw=lw, int_line_num=3, fontdict=font)
 
 for i in range(0, len(x1)):
 
@@ -96,6 +108,6 @@ for i in range(0, len(x1)):
 
     str_text = 'Epllise\nclockwise'
 
-    theta, pt = curbrac(axes[1], p1, p2, k_r, str_text=str_text, color=color, lw=lw, int_line_num=3, fontdict=font)
+    curlyBrace(fig, axes[1], p1, p2, k_r, bool_auto=True, str_text=str_text, color=color2, lw=lw, int_line_num=3, fontdict=font)
 
 plt.show()
