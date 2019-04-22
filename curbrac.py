53ee4f94ce22a552fd4c50d113e907116e4f2ab8
iruletheworld <siyu.gao@outlook.com>
2019-04-22 18:11:41 +0100

    refactored to consider axes of different scales and log scale axes

--- curbrac.py
+++ curbrac.py
@@ -1,45 +1,107 @@
 # -*- coding: utf-8 -*- 
 
 '''
+Note that you must transform the axis coordinates to screen coordinates!
+Otherwise you may not be able to see the arcs!
 '''
 
-# https://stackoverflow.com/questions/1289681/drawing-braces-with-pyx/1290110#1290110
 # https://uk.mathworks.com/matlabcentral/fileexchange/38716-curly-brace-annotation
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-def curbrac(ax, p1, p2, k_r=0.1, str_text='', int_line_num=2, fontdict={}, **kwargs):
+def get_ax_size(fig, ax):
+    '''
+    # https://stackoverflow.com/questions/19306510/determine-matplotlib-axis-size-in-pixels
+    '''
+
+    bbox = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+    ax_width, ax_height = bbox.width, bbox.height
+    ax_width *= fig.dpi
+    ax_height *= fig.dpi
+
+    return ax_width, ax_height #, fig_width, fig_height
+
+
+def curbrac(fig, ax, p1, p2, k_r=0.1, str_text='', int_line_num=2, fontdict={}, **kwargs):
     '''
     '''
+
+    pt1 = [None, None]
+    pt2 = [None, None]
+
+    ax_width, ax_height = get_ax_size(fig, ax)
+
+    ax_xlim = ax.get_xlim()
+    ax_ylim = ax.get_ylim()
+
+    if 'log' in ax.get_xaxis().get_scale():
+
+        pt1[0] = np.log(p1[0])
+
+        pt2[0] = np.log(p2[0])
+
+        ax_xlim = np.log(ax_xlim)
+
+        # print('log')
+
+    else:
+
+        pt1[0] = p1[0]
+        pt2[0] = p2[0]
+
+    if 'log' in ax.get_yaxis().get_scale():
+
+        pt1[1] = np.log(p1[1])
+
+        pt2[1] = np.log(p2[1])
+
+        ax_ylim = np.log(ax_ylim)
+
+    else:
+
+        pt1[1] = p1[1]
+        pt2[1] = p2[1]
+
+    # get the ratio of pixels/length
+    xscale = ax_width / (ax_xlim[1] - ax_xlim[0])
+    yscale = ax_height / (ax_ylim[1] - ax_ylim[0])
+
+    # convert length to pixels, 
+    # need to minus the lower limit to move the points back to the origin. Then add the limits back on end.
+    pt1[0] = (pt1[0] - ax_xlim[0]) * xscale
+    pt1[1] = (pt1[1] - ax_ylim[0]) * yscale
+    pt2[0] = (pt2[0] - ax_xlim[0]) * xscale
+    pt2[1] = (pt2[1] - ax_ylim[0]) * yscale
 
     # calculate the angle
-    theta = np.arctan2(p2[1] - p1[1], p2[0] - p1[0])
+    theta = np.arctan2(pt2[1] - pt1[1], pt2[0] - pt1[0])
 
     # calculate the radius of the arcs
-    r = np.hypot(p2[0] - p1[0], p2[1] - p1[1]) * k_r
+    r = np.hypot(pt2[0] - pt1[0], pt2[1] - pt1[1]) * k_r
 
     # arc1 centre
-    x11 = p1[0] + r * np.cos(theta)
-    y11 = p1[1] + r * np.sin(theta)
+    x11 = pt1[0] + r * np.cos(theta)
+    y11 = pt1[1] + r * np.sin(theta)
 
     # arc2 centre
-    x22 = (p2[0] + p1[0]) / 2.0 - 2.0 * r * np.sin(theta) - r * np.cos(theta)
-    y22 = (p2[1] + p1[1]) / 2.0 + 2.0 * r * np.cos(theta) - r * np.sin(theta)
+    x22 = (pt2[0] + pt1[0]) / 2.0 - 2.0 * r * np.sin(theta) - r * np.cos(theta)
+    y22 = (pt2[1] + pt1[1]) / 2.0 + 2.0 * r * np.cos(theta) - r * np.sin(theta)
 
     # arc3 centre
-    x33 = (p2[0] + p1[0]) / 2.0 - 2.0 * r * np.sin(theta) + r * np.cos(theta)
-    y33 = (p2[1] + p1[1]) / 2.0 + 2.0 * r * np.cos(theta) + r * np.sin(theta)
+    x33 = (pt2[0] + pt1[0]) / 2.0 - 2.0 * r * np.sin(theta) + r * np.cos(theta)
+    y33 = (pt2[1] + pt1[1]) / 2.0 + 2.0 * r * np.cos(theta) + r * np.sin(theta)
 
     # arc4 centre
-    x44 = p2[0] - r * np.cos(theta)
-    y44 = p2[1] - r * np.sin(theta)
+    x44 = pt2[0] - r * np.cos(theta)
+    y44 = pt2[1] - r * np.sin(theta)
 
     # prepare the rotated
     q = np.linspace(theta, theta + np.pi/2.0, 50)
 
     # reverse q
-    t = np.flip(q)
+    # t = np.flip(q) # this command is not supported by lower version of numpy
+    t = q[::-1]
 
     # arc coordinates
     arc1x = r * np.cos(t + np.pi/2.0) + x11
@@ -53,6 +115,48 @@ def curbrac(ax, p1, p2, k_r=0.1, str_text='', int_line_num=2, fontdict={}, **kwa
 
     arc4x = r * np.cos(t) + x44
     arc4y = r * np.sin(t) + y44
+
+    # convert back to the axis coordinates
+    arc1x = arc1x / xscale + ax_xlim[0]
+    arc2x = arc2x / xscale + ax_xlim[0]
+    arc3x = arc3x / xscale + ax_xlim[0]
+    arc4x = arc4x / xscale + ax_xlim[0]
+
+    arc1y = arc1y / yscale + ax_ylim[0]
+    arc2y = arc2y / yscale + ax_ylim[0]
+    arc3y = arc3y / yscale + ax_ylim[0]
+    arc4y = arc4y / yscale + ax_ylim[0]
+
+    if 'log' in ax.get_xaxis().get_scale():
+
+        # arc1x = np.power(10, arc1x)
+        # arc2x = np.power(10, arc2x)
+        # arc3x = np.power(10, arc3x)
+        # arc4x = np.power(10, arc4x)
+        arc1x = np.exp(arc1x)
+        arc2x = np.exp(arc2x)
+        arc3x = np.exp(arc3x)
+        arc4x = np.exp(arc4x)
+
+    else:
+
+        pass
+
+    if 'log' in ax.get_yaxis().get_scale():
+
+        # arc1y = np.power(10, arc1y)
+        # arc2y = np.power(10, arc2y)
+        # arc3y = np.power(10, arc3y)
+        # arc4y = np.power(10, arc4y)
+
+        arc1y = np.exp(arc1y)
+        arc2y = np.exp(arc2y)
+        arc3y = np.exp(arc3y)
+        arc4y = np.exp(arc4y)
+
+    else:
+
+        pass
 
     # plot arcs
     ax.plot(arc1x, arc1y, **kwargs)
@@ -72,7 +176,7 @@ def curbrac(ax, p1, p2, k_r=0.1, str_text='', int_line_num=2, fontdict={}, **kwa
 
         str_temp = '\n' * int_line_num
         
-        # conver radians to degree and within 0 to 360
+        # convert radians to degree and within 0 to 360
         ang = np.degrees(theta) % 360.0
 
         if (ang >= 0.0) and (ang <= 90.0):
@@ -98,5 +202,10 @@ def curbrac(ax, p1, p2, k_r=0.1, str_text='', int_line_num=2, fontdict={}, **kwa
     else:
 
         pass
+
+    arc1 = [arc1x, arc1y]
+    arc2 = [arc2x, arc2y]
+    arc3 = [arc3x, arc3y]
+    arc4 = [arc4x, arc4y]
 
     return theta, summit
