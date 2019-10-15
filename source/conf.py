df4bbaeb76e3c1916838454d6d79b4c27747d381
iruletheworld <siyu.gao@outlook.com>
2019-10-15 01:05:24 +0100

    need sphinx.ext.napoleon ?

--- source/conf.py
+++ source/conf.py
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
